407dd5df1c773c95fd5af3b63a18935edd379e97
ChirayuGupta <chirayugupta2003@gmail.com>
2022-03-04 15:01:18 +0530

    Update Lab2A1.cpp

--- Semester 2/Lab2/Lab2A1.cpp
+++ Semester 2/Lab2/Lab2A1.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 class test {
-    
+    int private_var = 69; // private variable
+public:
+    int public_var = 6969; // public variable
 };
+
+int main() {
+    test test;
+//    test.private_var       gives error
+    cout << test.public_var;
+}
