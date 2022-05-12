2d815ef4dba8d04caaf820b19bcaa64c52f50ba1
ChirayuGupta <87424491+ChirayuGupta2003@users.noreply.github.com>
2022-05-12 23:50:49 +0530

    commit

---
+++ Semester 2/AssignmentSet2/Set2Lab6/Set2Lab6A1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class triangle {
+
+    float area(float base, float height, float hypotenuse) {
+        float area = base * height / 2;
+        return area;
+    }
+
+    float area(float base, float side) {
+        float height;
+    }
+};
